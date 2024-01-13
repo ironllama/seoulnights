5c81447fd4a808febad743cedb7c458d36ca5bb6
Jason <jasonchoi13@gmail.com>
2024-01-13 17:54:32 +0900

    adding all the stuff from saturday
    adding in battle completion
    adding in cullens convenience

--- map/getRoundResult.php
+++ map/getRoundResult.php
@@ -14,63 +14,84 @@ try {
 
 $session_id = session_id();
 
-$jsonData = file_get_contents('php://input');
-$roundData = json_decode($jsonData, true);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $jsonData = file_get_contents('php://input');
 
-// $roundData[0];  < --- this is the entire object for the played card
-// $roundData[1];  < --- this is the enemy move object
+    if (!empty($jsonData)) {
+        $roundData = json_decode($jsonData, true);
 
-//parsing for the individual battle stats of player and enemy
-$card_attack = $roundData[0]['card_attack'];
-$card_defense = $roundData[0]['card_defense'];
-$card_regen = $roundData[0]['card_regen'];
+        // $roundData[0];  < --- this is the entire object for the played card
+        // $roundData[1];  < --- this is the enemy move object
 
-$enemyMove_attack = $roundData[1]['move_attack'];
-$enemyMove_defense = $roundData[1]['move_defend'];
-$enemyMove_regen = $roundData[1]['move_regen'];
+        //parsing for the individual battle stats of player and enemy
+        $card_attack = $roundData[0]['card_attack'];
+        $card_defense = $roundData[0]['card_defense'];
+        $card_regen = $roundData[0]['card_regen'];
 
-//getting current gamestate on card click
-$currentGameState = $db->prepare("SELECT * FROM gameplay_logs where run_sessionID = '$session_id'");
-$currentGameState->execute();
-$playerStats = $currentGameState->fetch(PDO::FETCH_ASSOC);
+        $enemyMove_attack = $roundData[1]['move_attack'];
+        $enemyMove_defense = $roundData[1]['move_defend'];
+        $enemyMove_regen = $roundData[1]['move_regen'];
 
-$playerEnergyLevel = $playerStats['run_energyLevel']; // current energy levels that were found on click
-$playerMoneyLevel = $playerStats['run_moneyLevel'];
-$playerDrunkLevel = $playerStats['run_drunkLevel'];
+        //getting current gamestate on card click
+        $currentGameState = $db->prepare("SELECT * FROM gameplay_logs where run_sessionID = '$session_id'");
+        $currentGameState->execute();
+        $playerStats = $currentGameState->fetch(PDO::FETCH_ASSOC);
 
-// battle logic
+        $playerEnergyLevel = $playerStats['run_energyLevel']; // current energy levels that were found on click
+        $playerMoneyLevel = $playerStats['run_moneyLevel'];
+        $playerDrunkLevel = $playerStats['run_drunkLevel'];
 
-$takenPlayerDamage = $card_defense - $enemyMove_attack; // card defense - enemy attack
-$takenEnemyDamage = $enemyMove_defense - $card_attack; // enemy defense - card attack
-$totalPlayerRegen = $card_regen; // card regen value
-$totalEnemyRegen = $enemyMove_regen; // enemy regen value
+        // battle logic
+
+        $takenPlayerDamage = $card_defense - $enemyMove_attack; // card defense - enemy attack
+        $takenEnemyDamage = $enemyMove_defense - $card_attack; // enemy defense - card attack
+        $totalPlayerRegen = $card_regen; // card regen value
+        $totalEnemyRegen = $enemyMove_regen; // enemy regen value
 
 
-if ($takenPlayerDamage >= 0) { // if defense is higher
-    $takenPlayerDamage = 0; // players takes 0 damage
-} else $takenPlayerDamage; // otherwise takenplayerdamage 
+        if ($takenPlayerDamage >= 0) { // if defense is higher
+            $takenPlayerDamage = 0; // players takes 0 damage
+        } else $takenPlayerDamage; // otherwise takenplayerdamage 
 
-if ($takenEnemyDamage >= 0) { // if enemy defense is higher
-    $takenEnemyDamage = 0; // enemy takes 0 damage
-} else $takenEnemyDamage; // otherwise takenenemydamage
+        if ($takenEnemyDamage >= 0) { // if enemy defense is higher
+            $takenEnemyDamage = 0; // enemy takes 0 damage
+        } else $takenEnemyDamage; // otherwise takenenemydamage
 
-$roundPlayerEnergy = ($takenPlayerDamage + $totalPlayerRegen);
-$roundEnemyEnergy = ($takenEnemyDamage + $totalEnemyRegen);
+        $roundPlayerEnergy = ($takenPlayerDamage + $totalPlayerRegen);
+        $roundEnemyEnergy = ($takenEnemyDamage + $totalEnemyRegen);
 
-if ($playerEnergyLevel + $takenPlayerDamage > $playerEnergyLevel) {
-    $playerHealth = $playerEnergyLevel;
+        if ($playerEnergyLevel + $takenPlayerDamage > $playerEnergyLevel) {
+            $playerHealth = $playerEnergyLevel;
+        } else {
+            $updatedEnergy = ($playerEnergyLevel + $takenPlayerDamage);
+            $playerHealth = $updatedEnergy;
+        }
+
+        $updatePlayerEnergy = $db->prepare("UPDATE gameplay_logs set run_energyLevel = '$playerHealth' where run_sessionID = '$session_id'");
+        $updatePlayerEnergy->execute();
+
+        $results = array(
+            'updatedEnergyLevel' => $playerHealth,
+            'updatedMoneyLevel' => $playerMoneyLevel,
+            'updatedDrunkLevel' => $playerDrunkLevel
+        );
+
+        echo json_encode($results);
+    } else echo "Nothing was sent";
 } else {
-    $updatedEnergy = ($playerEnergyLevel + $takenPlayerDamage);
-    $playerHealth = $updatedEnergy;
+    $currentGameState = $db->prepare("SELECT * FROM gameplay_logs where run_sessionID = '$session_id'");
+    $currentGameState->execute();
+    $playerStats = $currentGameState->fetch(PDO::FETCH_ASSOC);
+
+    $playerEnergyLevel = $playerStats['run_energyLevel'];
+    $playerMoneyLevel = $playerStats['run_moneyLevel'];
+    $playerDrunkLevel = $playerStats['run_drunkLevel'];
+
+    $results = array(
+        'updatedEnergyLevel' => $playerEnergyLevel,
+        'updatedMoneyLevel' => $playerMoneyLevel,
+        'updatedDrunkLevel' => $playerDrunkLevel
+    );
+
+    echo json_encode($results);
 }
-
-$updatePlayerEnergy = $db->prepare("UPDATE gameplay_logs set run_energyLevel = '$playerHealth' where run_sessionID = '$session_id'");
-$updatePlayerEnergy->execute();
-
-$results = array(
-    'updatedEnergyLevel' => $playerHealth,
-    'updatedMoneyLevel' => $playerMoneyLevel,
-    'updatedDrunkLevel' => $playerDrunkLevel
-);
-
-echo json_encode($results);
